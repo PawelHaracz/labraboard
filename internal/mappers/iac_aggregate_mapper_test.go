9ec471196dad87d4e5516acc2e7f3de29973d714
pawelharacz <haracz5@live.com>
2024-04-13 23:11:59 +0200

    Day 44/100 commits - Create new mapper test files

---
+++ internal/mappers/iac_aggregate_mapper_test.go
@@ -0,0 +1,11 @@
+package mappers
+
+import "testing"
+
+func TestIacMapper_Map(t *testing.T) {
+
+}
+
+func TestIacMapper_RevertMap(t *testing.T) {
+
+}
---
+++ internal/mappers/iac_plan_mappert_test.go
@@ -0,0 +1,11 @@
+package mappers
+
+import "testing"
+
+func TestIacPlanMapper_Map(t *testing.T) {
+
+}
+
+func TestIacPlanMapper_RevertMap(t *testing.T) {
+
+}
